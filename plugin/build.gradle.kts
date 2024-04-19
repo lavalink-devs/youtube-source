79e604fed74202684d51b8ce3961dfb1d647bd3f
devoxin <lukel9914@gmail.com>
2024-04-19 21:52:28 +0100

    :common and :lldevs should be implementation

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 val moduleName = "plugin"
 
 dependencies {
+    implementation(project(":common"))
+    implementation(project(":lldevs"))
     compileOnly("dev.arbjerg.lavalink:plugin-api:3.7.11")
     compileOnly("dev.arbjerg.lavalink:Lavalink-Server:3.7.11")
     compileOnly("dev.arbjerg:lavaplayer-ext-youtube-rotator:1.5.3")
-    compileOnly(project(":common"))
-    compileOnly(project(":lldevs"))
 }
 
 java {
