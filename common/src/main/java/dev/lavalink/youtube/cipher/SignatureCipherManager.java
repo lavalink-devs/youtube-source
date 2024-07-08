bb4a676fcbd81f6948e647b685936800e4156415
Devoxin <lukel9914@gmail.com>
2024-07-08 22:01:34 +0100

    Remove unused import

--- common/src/main/java/dev/lavalink/youtube/cipher/SignatureCipherManager.java
+++ common/src/main/java/dev/lavalink/youtube/cipher/SignatureCipherManager.java
@@ -23,7 +23,6 @@ import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
