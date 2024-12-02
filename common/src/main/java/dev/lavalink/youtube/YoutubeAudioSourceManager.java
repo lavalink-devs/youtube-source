502753aeea174f909fcf2b5a431b1d7f54be7cc6
Jirayu <jirayu.s@jirayu.net>
2024-12-03 01:55:44 +0700

    chore: update default clients

--- common/src/main/java/dev/lavalink/youtube/YoutubeAudioSourceManager.java
+++ common/src/main/java/dev/lavalink/youtube/YoutubeAudioSourceManager.java
@@ -79,7 +79,7 @@ public class YoutubeAudioSourceManager implements AudioSourceManager {
 
     public YoutubeAudioSourceManager(boolean allowSearch, boolean allowDirectVideoIds, boolean allowDirectPlaylistIds) {
         // query order: music -> web -> androidtestsuite -> tvhtml5embedded
-        this(allowSearch, allowDirectVideoIds, allowDirectPlaylistIds, new Music(), new Web(), new AndroidTestsuite(), new TvHtml5Embedded());
+        this(allowSearch, allowDirectVideoIds, allowDirectPlaylistIds, new Music(), new AndroidVr(), new Web(), new WebEmbedded());
     }
 
     /**
