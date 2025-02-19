d8908a90d0b32417efdc08ff7542e5a7ab3cdb1c
Devoxin <lukel9914@gmail.com>
2025-02-19 17:37:01 +0000

    Allow setting pot to all supporting clients with one method call

--- common/src/main/java/dev/lavalink/youtube/YoutubeSource.java
+++ common/src/main/java/dev/lavalink/youtube/YoutubeSource.java
@@ -1,9 +1,16 @@
 package dev.lavalink.youtube;
 
+import dev.lavalink.youtube.clients.Web;
+import dev.lavalink.youtube.clients.WebEmbedded;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 
 public class YoutubeSource {
+    private static final Logger log = LoggerFactory.getLogger(YoutubeSource.class);
+
     public static String VERSION = "Unknown";
 
     static {
@@ -21,5 +28,19 @@ public class YoutubeSource {
         } catch (IOException ignored) {
 
         }
+    }
+
+    /**
+     * Sets the given PoToken and VisitorData pair on all POT-supporting clients.
+     * This is a convenience method to allow for setting this from one method call.
+     * @param poToken The poToken to use. This must be paired to the specified visitorData.
+     *                You may specify {@code null} to unset.
+     * @param visitorData The visitorData to use. This must be paired to the specified poToken.
+     *                    You may specify {@code null} to unset.
+     */
+    public static void setPoTokenAndVisitorData(String poToken, String visitorData) {
+        log.debug("Applying pot: {} vd: {} to WEB, WEBEMBEDDED", poToken, visitorData);
+        Web.setPoTokenAndVisitorData(poToken, visitorData);
+        WebEmbedded.setPoTokenAndVisitorData(poToken, visitorData);
     }
 }
